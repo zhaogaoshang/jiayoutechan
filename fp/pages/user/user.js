93d017b15a4b61e6313fa2c24b309f0e31804786
zhaogaoshang <zhaogaoshang@163.com>
2019-01-25 16:33:03 +0800

    user over

--- fp/pages/user/user.js
+++ fp/pages/user/user.js
@@ -5,62 +5,80 @@ Page({
    * 页面的初始数据
    */
   data: {
-    pickList:[{}]
+    pickList: [{
+      name: '我的邀请',
+      id: 0
+    }, {
+      name: '我为家乡代言',
+      id: 1
+    }, {
+      name: '我的收藏',
+      id: 2
+    }, {
+      name: '我的优惠券',
+      id: 3
+    }, {
+      name: '我的地址',
+      id: 4
+    }, {
+      name: '我要开店',
+      id: 5
+    }, ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
